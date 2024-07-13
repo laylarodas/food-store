793d29880b653ae69638406397c76f632d1d03a1
Layla Rodas <rodas.layla@gmail.com>
2024-07-13 14:59:02 +0200

    add data

---
+++ prisma/data/categories.ts
@@ -0,0 +1,26 @@
+export const categories = [
+    {
+        slug: "coffee",
+        name: "Coffee"
+    },
+    {
+        slug: "burger",
+        name: "Burgers"
+    },
+    {
+        slug: "pizza",
+        name: "Pizzas"
+    },
+    {
+        slug: "dona",
+        name: "Donuts"
+    },
+    {
+        slug: "pastel",
+        name: "Cakes"
+    },
+    {
+        slug: "cookie",
+        name: "Cookies"
+    }
+];
