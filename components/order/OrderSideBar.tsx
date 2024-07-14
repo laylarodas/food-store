b0013c219160f0140265e8ad4ccd0983d6eca596
Layla Rodas <rodas.layla@gmail.com>
2024-07-14 15:33:56 +0200

    get categories

--- components/order/OrderSideBar.tsx
+++ components/order/OrderSideBar.tsx
@@ -1,5 +1,15 @@
+import { prisma } from '@/src/lib/prisma'
 
-export default function OrderSideBar() {
+
+
+async function getCategories() {
+    return await prisma.category.findMany()
+}
+
+export default async function OrderSideBar() {
+
+    const categories = await getCategories()
+    console.log(categories)
     return (
         <aside className=" md:w-72 md:h-screen bg-white">
             OrderSideBar
