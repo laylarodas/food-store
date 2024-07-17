3f6cb456c6690cafdef5956ef2ce4de2fca1c989
Layla Rodas <rodas.layla@gmail.com>
2024-07-17 23:30:14 +0200

    add state - add to order

--- components/products/ProductCard.tsx
+++ components/products/ProductCard.tsx
@@ -1,6 +1,7 @@
 import { formatCurrency } from "@/src/utils"
 import { Product } from "@prisma/client"
 import Image from "next/image"
+import AddProductButton from "./AddProductButton"
 
 
 type ProductCardProps = {
@@ -19,10 +20,10 @@ export default function ProductCard({product}: ProductCardProps) {
             <div className=" p-5">
                 <h3 className="text-xl font-semibold">{product.name}</h3>
                 <p className=" mt-5 font-bold text-2xl text-amber-500">{ formatCurrency(product.price)}</p>
-                <button type="button" className=" bg-indigo-600 hover:bg-indigo-800 text-white w-full mt-5 p-3 uppercase font-semibold cursor-pointer">
-                    Add to Order
-                </button>
+                
             </div>
+
+            <AddProductButton product={product}/>
         </div>
     )
 }
