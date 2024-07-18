78fc573bbe40c5da5e9c153566e70eda7a28f931
Layla Rodas <rodas.layla@gmail.com>
2024-07-18 11:22:55 +0200

    get formdata with actions

--- components/order/OrderSummary.tsx
+++ components/order/OrderSummary.tsx
@@ -9,10 +9,11 @@ import { createOrder } from "@/actions/create-order-action"
 export default function OrderSummary() {
 
   const order = useStore((state) => state.order)
-  const total = useMemo(()=> order.reduce((total, item) => total + (item.quantity * item.price), 0), [order])
+  const total = useMemo(() => order.reduce((total, item) => total + (item.quantity * item.price), 0), [order])
 
 
-  const handleCreateOrder = () => {
+  const handleCreateOrder = (formData: FormData) => {
+    console.log(formData.get('name'))
     createOrder()
   }
 
@@ -20,24 +21,35 @@ export default function OrderSummary() {
 
   return (
     <aside className=" lg:h-screen lg:overflow-y-scroll md:w-64 lg:w-96 p-5">
-        <h1 className=" text-4xl text-center font-black ">My Order</h1>
+      <h1 className=" text-4xl text-center font-black ">My Order</h1>
 
-        {order.length === 0 ? <p className="text-center my-10">The order is empty</p> : (
-          <div className="mt-5">
-            {order.map( item => (
-              <ProductDetails key={item.id} item={item}/>
-            ))}
+      {order.length === 0 ? <p className="text-center my-10">The order is empty</p> : (
+        <div className="mt-5">
+          {order.map(item => (
+            <ProductDetails key={item.id} item={item} />
+          ))}
 
-            <p className="text-xl mt-20 text-center">
-              Total to pay: {''}
-              <span className="font-bold">{formatCurrency(total)}</span>
-            </p>
+          <p className="text-xl mt-20 text-center">
+            Total to pay: {''}
+            <span className="font-bold">{formatCurrency(total)}</span>
+          </p>
 
-            <form action={handleCreateOrder} className=" w-full mt-10 space-y-5">
-              <input type="submit" value="Confirm Order" className="py-2 rounded uppercase text-white bg-black w-full text-center cursor-pointer font-bold" />
-            </form>
-          </div>
-        )}
+          <form action={handleCreateOrder} className=" w-full mt-10 space-y-5">
+            <input
+              type="text"
+              placeholder="Your Name"
+              className=" bg-white border-gray-100 p-2 w-full"
+              name="name"
+            />
+            <input
+              type="submit"
+              value="Confirm Order"
+              className="py-2 rounded uppercase text-white bg-black w-full text-center cursor-pointer font-bold"
+              
+            />
+          </form>
+        </div>
+      )}
     </aside>
   )
 }
