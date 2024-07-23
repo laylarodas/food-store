c1b5cb8c5ac083a1ede7d66f81642eaa52f94a6b
Layla Rodas <rodas.layla@gmail.com>
2024-07-23 23:37:06 +0200

    show pending orders

--- app/admin/orders/page.tsx
+++ app/admin/orders/page.tsx
@@ -1,3 +1,4 @@
+import OrderCard from '@/components/order/OrderCard'
 import Heading from '@/components/ui/Heading'
 import { prisma } from '@/src/lib/prisma'
 
@@ -27,6 +28,14 @@ export default async function OrdersPage() {
   return (
     <>
       <Heading>Manage Orders</Heading>
+
+      {orders.length ? (
+        <div className=' grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-3 gap-5 mt-5'>
+            {orders.map(order => (
+              <OrderCard key={order.id} order={order}/>
+            ))}
+        </div>
+      ): <p className=' text-center'> There are not pending orders</p> }
     </>
   )
 }
---
+++ components/order/OrderCard.tsx
@@ -0,0 +1,35 @@
+import { OrderWithProducts } from "@/src/types"
+
+type OrderCardProps = {
+    order: OrderWithProducts
+}
+
+
+
+export default function OrderCard({ order }: OrderCardProps) {
+
+    return (
+        <section
+            aria-labelledby="summary-heading"
+            className="mt-16 rounded-lg bg-gray-50 px-4 py-6 sm:p-6  lg:mt-0 lg:p-8 space-y-4"
+        >
+            <p className='text-2xl font-medium text-gray-900'>Customer: </p>
+            <p className='text-lg font-medium text-gray-900'>Order Products:</p>
+            <dl className="mt-6 space-y-4">
+        
+                <div className="flex items-center justify-between border-t border-gray-200 pt-4">
+                    <dt className="text-base font-medium text-gray-900">Total to pay:</dt>
+                    <dd className="text-base font-medium text-gray-900">{}</dd>
+                </div>
+            </dl>
+
+            <form>
+                <input
+                    type="submit"
+                    className="bg-indigo-600 hover:bg-indigo-800 text-white w-full mt-5 p-3 uppercase font-bold cursor-pointer"
+                    value='Mark order completed'
+                />
+            </form>
+        </section>
+    )
+}
