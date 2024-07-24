dad4f6c0ec5a2725622d1a639d376184a9f2c06e
Layla Rodas <rodas.layla@gmail.com>
2024-07-24 10:30:30 +0200

    show order details

--- components/order/OrderCard.tsx
+++ components/order/OrderCard.tsx
@@ -1,4 +1,5 @@
 import { OrderWithProducts } from "@/src/types"
+import { formatCurrency } from "@/src/utils"
 
 type OrderCardProps = {
     order: OrderWithProducts
@@ -13,12 +14,19 @@ export default function OrderCard({ order }: OrderCardProps) {
             aria-labelledby="summary-heading"
             className="mt-16 rounded-lg bg-gray-50 px-4 py-6 sm:p-6  lg:mt-0 lg:p-8 space-y-4"
         >
-            <p className='text-2xl font-medium text-gray-900'>Customer: </p>
+            <p className='text-2xl font-medium text-gray-900'>Customer: {order.name} </p>
             <p className='text-lg font-medium text-gray-900'>Order Products:</p>
             <dl className="mt-6 space-y-4">
-        
+                {order.orderProducts.map(product => (
+                    <div key={product.id} className=" flex items-center gap-2 border-t border-gray-200 pt-4">
+                        <dt className="flex items-center text-sm text-gray-600">
+                            <span className="font-black">{product.quantity}</span>
+                        </dt>
+                        <dd className="text-sm font-medium text-gray-900">{product.product.name}</dd>
+                    </div>
+                ))}
                 <div className="flex items-center justify-between border-t border-gray-200 pt-4">
-                    <dt className="text-base font-medium text-gray-900">Total to pay:</dt>
+                    <dt className="text-base font-medium text-gray-900">Total to pay: {formatCurrency(order.total)}</dt>
                     <dd className="text-base font-medium text-gray-900">{}</dd>
                 </div>
             </dl>
