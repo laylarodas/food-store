7a7e612d7c9346f55735408d013c59d2063656d7
Layla Rodas <rodas.layla@gmail.com>
2024-07-25 23:54:35 +0200

    show products table

--- app/admin/products/page.tsx
+++ app/admin/products/page.tsx
@@ -1,11 +1,26 @@
+import ProductTable from "@/components/products/ProductTable";
 import Heading from "@/components/ui/Heading";
+import { prisma } from "@/src/lib/prisma";
 
 
-export default function ProductsPage() {
+async function getProducts() {
+  const products = await prisma.product.findMany()
+
+  return products
+}
+
+export default async function ProductsPage() {
+
+  const products = await getProducts()
+
+  console.log(products)
+
 
   return (
     <>
       <Heading>Manage Products</Heading>
+
+      <ProductTable products={products}/>
     </>
   )
 }
