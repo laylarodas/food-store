9c833f7d55c4add6422dd4abfa8208a35d3a704a
Layla Rodas <rodas.layla@gmail.com>
2024-07-27 15:11:14 +0200

    pagination with numbers

--- components/products/ProductsPagination.tsx
+++ components/products/ProductsPagination.tsx
@@ -7,6 +7,9 @@ type ProductsPaginationProps = {
 }
 
 export default function ProductsPagination({ page, totalPages }: ProductsPaginationProps) {
+    const pages = Array.from({ length: totalPages }, (_, i) => i + 1)
+
+    
     return (
         <nav className='flex justify-center py-10'>
 
@@ -17,6 +20,12 @@ export default function ProductsPagination({ page, totalPages }: ProductsPaginat
                 >&laquo;</Link>
             )}
 
+            {pages.map(currentPage => (
+                <Link
+                    href={`/admin/products?page=${currentPage}`}
+                    className={`${page === currentPage && 'font-bold underline underline-offset-2'} bg-white px-4 py-2 text-sm text-gray-900 ring-1 ring-inset ring-gray-300 focus:z-20 focus:outline-offset-0`}
+                >{currentPage}</Link>
+            ))}
 
             {page < totalPages && (
                 <Link
