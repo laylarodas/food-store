fbc79ed92a921ded76cb3ec0eb6b53def46ba836
Layla Rodas <rodas.layla@gmail.com>
2024-07-27 14:59:40 +0200

    pagination - prev next

--- app/admin/products/page.tsx
+++ app/admin/products/page.tsx
@@ -1,10 +1,11 @@
+import { redirect } from "next/navigation";
 import ProductsPagination from "@/components/products/ProductsPagination";
 import ProductTable from "@/components/products/ProductTable";
 import Heading from "@/components/ui/Heading";
 import { prisma } from "@/src/lib/prisma";
 
 
-async function productCount( ) {
+async function productCount() {
   return await prisma.product.count()
 }
 
@@ -34,13 +35,15 @@ export default async function ProductsPage({ searchParams }: { searchParams: { p
 
   const page = +searchParams.page || 1
   const pageSize = 10
- 
 
+  if (page < 0) redirect('/admin/products')
 
   const productsData = await getProducts(page, pageSize)
   const totalProductsData = await productCount()
-  const [ products, totalProducts] = await Promise.all([productsData, totalProductsData])
+  const [products, totalProducts] = await Promise.all([productsData, totalProductsData])
   const totalPages = Math.ceil(totalProducts / pageSize)
+
+  if (page > totalPages) redirect('/admin/products')
 
 
   return (
@@ -49,7 +52,7 @@ export default async function ProductsPage({ searchParams }: { searchParams: { p
 
       <ProductTable products={products} />
 
-      <ProductsPagination page={page} totalPages={totalPages}/>
+      <ProductsPagination page={page} totalPages={totalPages} />
     </>
   )
 }
--- components/products/ProductsPagination.tsx
+++ components/products/ProductsPagination.tsx
@@ -10,8 +10,16 @@ export default function ProductsPagination({ page, totalPages }: ProductsPaginat
     return (
         <nav className='flex justify-center py-10'>
 
+            {page > 1 && (
+                <Link
+                    href={`/admin/products?page=${page - 1}`}
+                    className=' bg-white px-4 py-2 text-sm text-gray-900 ring-1 ring-inset ring-gray-300 focus:z-20 focus:outline-offset-0'
+                >&laquo;</Link>
+            )}
+
+
             {page < totalPages && (
-                <Link 
+                <Link
                     href={`/admin/products?page=${page + 1}`}
                     className=' bg-white px-4 py-2 text-sm text-gray-900 ring-1 ring-inset ring-gray-300 focus:z-20 focus:outline-offset-0'
                 >&raquo;</Link>
