e76faf5ce9fc59a9483cdb349ed4556d2fefb1ad
Layla Rodas <rodas.layla@gmail.com>
2024-07-29 12:54:49 +0200

    image upload component - cloudinary

--- components/products/ImageUpload.tsx
+++ components/products/ImageUpload.tsx
@@ -1,7 +1,39 @@
-import React from 'react'
+"use client"
+import { CldUploadWidget } from "next-cloudinary"
+import { TbPhotoPlus } from "react-icons/tb"
+
 
 export default function ImageUpload() {
-  return (
-    <div>ImageUpload</div>
-  )
+    return (
+        <CldUploadWidget
+            onSuccess={(result, { widget }) => {
+                console.log(result)
+            }}
+            uploadPreset="tnaz0o7k"
+            options={{
+                maxFiles: 1,
+
+            }}
+        >
+            {({ open }) => (
+                <>
+                    <div className=" space-y-2">
+                        <label className=" text-slate-800">Image Product</label>
+                        <div
+                            onClick={() => open()}
+                            className=" relative cursor-pointer hover:opacity-70 transition p-10 border-neutral-300 flex flex-col justify-center items-center gap-4 text-neutral-600 bg-slate-100"
+                        >
+                            <TbPhotoPlus
+                                size={50}
+                            />
+
+                            <p className=" text-lg font-semibold">Add image</p>
+
+
+                        </div>
+                    </div>
+                </>
+            )}
+        </CldUploadWidget>
+    )
 }
