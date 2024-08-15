53e3eb58581adfac656def8058e3e3cdabe2c9f3
Layla Rodas <rodas.layla@gmail.com>
2024-08-15 10:57:27 +0200

    ready orders - API

---
+++ app/orders/page.tsx
@@ -0,0 +1,12 @@
+import Logo from '@/components/ui/Logo'
+import React from 'react'
+
+export default function OrdersPage() {
+  return (
+    <>
+        <h1 className=' text-center mt-20 text-6xl font-black'>Ready Orders</h1>
+
+        <Logo />
+    </>
+  )
+}
